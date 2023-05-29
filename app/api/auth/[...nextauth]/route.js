23317ea3968e3013f54396611bae954df7155494
ayush-devshark <67559031+ayush-devshark@users.noreply.github.com>
2023-05-29 23:50:08 +0530

    add routes, models and user backend code

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,8 @@
-import  NextAuth  from 'next-auth';
+import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
+
+import User from '@models/user';
+import { connectToDB } from '@utils/database';
 
 const handler = NextAuth({
   providers: [
@@ -10,10 +13,33 @@ const handler = NextAuth({
   ],
   async session({ session }) {
     try {
+      const sessionUser = await User.findOne({ email: session.user.email });
+      session.id = sessionUser._id.toString();
+      return session;
     } catch (err) {}
   },
-  async signIn({ profile }) {},
+  async signIn({ profile }) {
+    try {
+      await connectToDB();
+
+      // check if a user already exists
+      const userExists = await User.findOne({ email: profile.email });
+
+      // if not, create a new user
+      if (!userExists) {
+        await User.create({
+          email: profile.email,
+          username: profile.name.replace(' ', '').toLowerCase(),
+          image: profile.picture,
+        });
+      }
+
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  },
 });
 
 export { handler as GET, handler as POST };
-
