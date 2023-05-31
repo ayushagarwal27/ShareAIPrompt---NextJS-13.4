4095631daa1d3b1ce6b60f63c240d3738061703c
ayush-devshark <67559031+ayush-devshark@users.noreply.github.com>
2023-05-31 19:38:21 +0530

    🐛 fix in connecting to mongoDB

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -11,34 +11,37 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  async session({ session }) {
-    try {
-      const sessionUser = await User.findOne({ email: session.user.email });
-      session.id = sessionUser._id.toString();
-      return session;
-    } catch (err) {}
-  },
-  async signIn({ profile }) {
-    try {
-      await connectToDB();
+  callbacks: {
+    async session({ session }) {
+      try {
+        const sessionUser = await User.findOne({ email: session.user.email });
+        session.id = sessionUser._id.toString();
+        return session;
+      } catch (err) {}
+    },
 
-      // check if a user already exists
-      const userExists = await User.findOne({ email: profile.email });
+    async signIn({ profile }) {
+      try {
+        await connectToDB();
 
-      // if not, create a new user
-      if (!userExists) {
-        await User.create({
-          email: profile.email,
-          username: profile.name.replace(' ', '').toLowerCase(),
-          image: profile.picture,
-        });
+        // check if a user already exists
+        const userExists = await User.findOne({ email: profile.email });
+
+        // if not, create a new user
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(' ', '').toLowerCase(),
+            image: profile.picture,
+          });
+        }
+
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
       }
-
-      return true;
-    } catch (error) {
-      console.log(error);
-      return false;
-    }
+    },
   },
 });
 
