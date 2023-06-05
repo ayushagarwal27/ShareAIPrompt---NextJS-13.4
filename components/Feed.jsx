50e902b19a720e2dbd44d2256258cc5088509062
ayush-devshark <67559031+ayush-devshark@users.noreply.github.com>
2023-06-05 19:54:38 +0530

    add feed component

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+'use client';
+
+import React, { useState, useEffect } from 'react';
+import PromptCard from './PromptCard';
+
+const PromptCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className='mt-16 prompt_layout'>
+      {data.map(post => {
+        return (
+          <PromptCard
+            key={post._id}
+            post={post}
+            handleTagClick={handleTagClick}
+          />
+        );
+      })}
+    </div>
+  );
+};
 
 const Feed = () => {
-  return (
-    <div>Feed</div>
-  )
-}
+  const [searchText, setSearchText] = useState('');
+  const [posts, setPosts] = useState([]);
+  const handleSearch = () => {};
 
-export default Feed
+  useEffect(() => {
+    const fetchPost = async () => {
+      const res = await fetch('/api/prompt');
+      const data = await res.json();
+      setPosts(data);
+    };
+    fetchPost();
+  }, []);
+
+  return (
+    <section className='feed'>
+      <form className='relative w-full flex-center '>
+        <input
+          type='text'
+          placeholder='search for a tag or a username'
+          value={searchText}
+          onChange={handleSearch}
+          required
+          className='search_input peer'
+        />
+      </form>
+      <PromptCardList data={posts} handleTagClick={() => {}} />
+    </section>
+  );
+};
+
+export default Feed;
