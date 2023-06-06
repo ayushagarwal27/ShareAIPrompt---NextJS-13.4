9a3bb89d0562689385ffa2c03db8d483e06bfe9c
ayush-devshark <67559031+ayush-devshark@users.noreply.github.com>
2023-06-06 16:34:43 +0530

    add to promptCard and clipboard functionality

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -6,6 +6,15 @@ import { useSession } from 'next-auth/react';
 import { usePathname, useRouter } from 'next/navigation';
 
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
+  const [copied, setCopied] = useState('');
+  const handleCopy = () => {
+    setCopied(post.prompt);
+    navigator.clipboard.writeText(post.prompt);
+
+    setTimeout(() =>{
+      setCopied('');
+    },3000)
+  };
   return (
     <div className='prompt_card'>
       <div className='flex justify-between items-start gap-5'>
@@ -25,10 +34,27 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
               {post?.creator?.email}
             </p>
           </div>
-
-         
+        </div>
+        <div className='copy_btn' onClick={handleCopy}>
+          <Image
+            src={
+              copied === post.prompt
+                ? '/assets/icons/tick.svg'
+                : '/assets/icons/copy.svg'
+            }
+            width={12}
+            height={12}
+            alt=''
+          />
         </div>
       </div>
+      <p className='my-4 font-satoshi text-sm text-gray-700'>{post.prompt}</p>
+      <p
+        className='font-inter text-sm blue_gradient cursor-pointer'
+        onClick={() => handleTagClick && handleTagClick(post.tag)}
+      >
+        {post.tag}
+      </p>
     </div>
   );
 };
